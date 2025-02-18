53ba25fce0ccc792ecd83e625e9a2a5830c1ce15
Samarquesan <sam.samarques28@gmail.com>
2025-02-18 17:29:10 -0300

    Anexação e remoção de tarefas na lista todo

---
+++ todo/src/App.jsx
@@ -0,0 +1,64 @@
+import { useState } from 'react';
+import Todo from './components/Todo.jsx';
+import TodoForm from './components/TodoForm.jsx';
+
+import "./App.css";
+
+
+function App() {
+  const [todos, setTodos] = useState([
+    {
+      id: 1,
+      text: "Criar funcionalidade X no sistema",
+      category: "Trabalho",
+      isCompleted: false,
+    },
+    {
+      id: 2,
+      text: "Ir para a academia",
+      category: "Pessoal",
+      isCompleted: false,
+    },
+    {
+      id: 3,
+      text: "Estudar React",
+      category: "Estudos",
+      isCompleted: false,
+    },
+  ]);
+
+  const addTodo =  (text, category) =>{
+
+      const newTodos = [...todos, {
+        id: Math.floor(Math.random() *10000),
+        text,
+        category,
+        isCompleted: false,
+      },
+    ];
+    
+    setTodos(newTodos);
+  };
+
+  const removeTodo = (id) =>{
+    const newTodos = [...todos]
+    const filteredTodos = newTodos.filter(todo =>
+       todo.id !== id ? todo : null
+      );
+      setTodos(filteredTodos);
+  }
+
+  return (
+    <div className='app'>
+      <h1>lista de Tarefas</h1>
+      <div className='todo-list'>
+        {todos.map((todo) => (
+          <Todo key={todo.id} todo={todo} removeTodo={removeTodo}/>
+        ))}
+      </div>
+      <TodoForm addTodo={addTodo}/>
+    </div>
+  );
+}
+
+export default App
